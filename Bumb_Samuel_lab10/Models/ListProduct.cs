22b2f1fdc3bb3d219b7c227724821370bdc15e25
Sami <bmb.sami@yahoo.com>
2021-11-20 18:26:51 +0200

    Add project files.

---
+++ Bumb_Samuel_lab10/Models/ListProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace  Bumb_Samuel_lab10.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+
+        public int ProductID { get; set; }
+    }
+}
